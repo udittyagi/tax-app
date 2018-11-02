4d5777ea3330536af18ee5806125d53212843908
udittyagi <udittyagi2@gmail.com>
2018-11-02 23:37:17 +0530

    Updates

---
+++ precache-manifest.3dafa8fd108db4a651a3267e1044ca46.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "716742d7301ba1c5731f",
+    "url": "/tax-app/static/css/main.f50eadf0.chunk.css"
+  },
+  {
+    "revision": "716742d7301ba1c5731f",
+    "url": "/tax-app/static/js/main.716742d7.chunk.js"
+  },
+  {
+    "revision": "40a65a1cd44b0edd15d6",
+    "url": "/tax-app/static/js/1.40a65a1c.chunk.js"
+  },
+  {
+    "revision": "f9bda940541015594c88",
+    "url": "/tax-app/static/js/runtime~main.f9bda940.js"
+  },
+  {
+    "revision": "e450665e4cb15e54a912ac302a5836de",
+    "url": "/tax-app/static/media/image_part_001.e450665e.jpg"
+  },
+  {
+    "revision": "9905b3a188885ca9f7e3c73424f11212",
+    "url": "/tax-app/static/media/image_part_002.9905b3a1.jpg"
+  },
+  {
+    "revision": "b954d3ed00c1f6c187a012b4cc53cc6d",
+    "url": "/tax-app/static/media/image_part_003.b954d3ed.jpg"
+  },
+  {
+    "revision": "0bd393df0131ee5ca621bdfeb243efa6",
+    "url": "/tax-app/static/media/image_part_004.0bd393df.jpg"
+  },
+  {
+    "revision": "a5d16c0733de227b610631efdffe7f09",
+    "url": "/tax-app/static/media/accounting-analytics-balance-209224-min.a5d16c07.jpg"
+  },
+  {
+    "revision": "020c36f5d852132e02a859e28e4fb0fc",
+    "url": "/tax-app/static/media/accounting-analytics-balance-209224.020c36f5.jpg"
+  },
+  {
+    "revision": "6767f4d1a89aee7b2b1538a8ddd899eb",
+    "url": "/tax-app/static/media/accounting-alone-application-938965-min.6767f4d1.jpg"
+  },
+  {
+    "revision": "765870a768d76d0f70fa3de5cb9f1b59",
+    "url": "/tax-app/static/media/architectural-design-architecture-building-443383.765870a7.jpg"
+  },
+  {
+    "revision": "64c098ef237adbc10621f72d7455ad2e",
+    "url": "/tax-app/index.html"
+  }
+];
