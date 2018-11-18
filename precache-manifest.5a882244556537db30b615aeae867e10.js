5bd93152acacf9208389b1556174ffc23feecc02
udittyagi <udittyagi2@gmail.com>
2018-11-18 18:12:48 +0530

    Updates

---
+++ precache-manifest.5a882244556537db30b615aeae867e10.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "13ee80fb20918e7c5795",
+    "url": "/tax-app/static/css/main.0604605e.chunk.css"
+  },
+  {
+    "revision": "13ee80fb20918e7c5795",
+    "url": "/tax-app/static/js/main.13ee80fb.chunk.js"
+  },
+  {
+    "revision": "f6af5d9de333d93f82e3",
+    "url": "/tax-app/static/js/1.f6af5d9d.chunk.js"
+  },
+  {
+    "revision": "f9bda940541015594c88",
+    "url": "/tax-app/static/js/runtime~main.f9bda940.js"
+  },
+  {
+    "revision": "04bbc5abf65dc1c924afdac2a93c9631",
+    "url": "/tax-app/static/media/image_part_001.04bbc5ab.jpg"
+  },
+  {
+    "revision": "3dda7ba85694028b141649efdb39dca9",
+    "url": "/tax-app/static/media/image_part_002.3dda7ba8.jpg"
+  },
+  {
+    "revision": "30ba1c46a78c5acd4d4dba17bb47dfb9",
+    "url": "/tax-app/static/media/image_part_003.30ba1c46.jpg"
+  },
+  {
+    "revision": "888eb577bf574107872deb9e2347c589",
+    "url": "/tax-app/static/media/image_part_004.888eb577.jpg"
+  },
+  {
+    "revision": "cb333b6e1cbc3ac9499887052f86383d",
+    "url": "/tax-app/static/media/calc.cb333b6e.jpeg"
+  },
+  {
+    "revision": "5b77bde4b84784459d2eab08377496cf",
+    "url": "/tax-app/static/media/mainImage.5b77bde4.jpeg"
+  },
+  {
+    "revision": "9b547ce5e8e378d55b6b112aff0063b8",
+    "url": "/tax-app/static/media/building.9b547ce5.jpeg"
+  },
+  {
+    "revision": "7c6821957817e6a9c4dd447a0767421c",
+    "url": "/tax-app/static/media/confidentiality.7c682195.jpg"
+  },
+  {
+    "revision": "ff7963a7440b2d2837f53f146652fb87",
+    "url": "/tax-app/static/media/team.ff7963a7.jpg"
+  },
+  {
+    "revision": "db0b960b6824c46d5ae6b6574e3ce5aa",
+    "url": "/tax-app/static/media/pricing.db0b960b.jpg"
+  },
+  {
+    "revision": "e09dd23c9dcb16bc30d39c7613279e4d",
+    "url": "/tax-app/static/media/experience.e09dd23c.jpg"
+  },
+  {
+    "revision": "0f02cf71563a935da9608a7443316ca5",
+    "url": "/tax-app/static/media/turnaround.0f02cf71.jpg"
+  },
+  {
+    "revision": "e17119857e6d0a767ffc6535a2d42774",
+    "url": "/tax-app/static/media/ongoingWork.e1711985.jpg"
+  },
+  {
+    "revision": "4ffd56fa988f80e339b12bbd49d1e130",
+    "url": "/tax-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tax-app/precache-manifest.c4551bfafeb40bf4ddf1c05d4ac70122.js"
+  "/tax-app/precache-manifest.5a882244556537db30b615aeae867e10.js"
 );
 
 workbox.clientsClaim();
